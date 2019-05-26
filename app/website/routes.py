61162672d949d1d1cfc47ddf470799c789ea2e22
c2gahlot <c2gahlot@gmail.com>
2019-05-26 22:24:25 +0530

    Added code that Persist that short URL

--- app/website/routes.py
+++ app/website/routes.py
@@ -4,7 +4,7 @@ import string
 import random
 from os.path import join, dirname, realpath
 
-STORAGE_FOLDER = join(dirname(realpath(__file__)), 'storage/')
+STORAGE_FILE = join(dirname(realpath(__file__)), 'storage/shorten_urls')
 BASE_URL = 'http://127.0.0.1:5000/'
 
 @website.route('/', methods=['GET', 'POST'])
@@ -13,31 +13,42 @@ def homepage():
     Render the homepage templates on the / route
     """
     if request.method == 'POST':
-        file_name = ''.join(random.choices(string.ascii_lowercase + string.digits, k=5))
-        file_path = STORAGE_FOLDER + file_name
+        url = request.form['url']
+        url_mappings = {}
+        with open(STORAGE_FILE, "r") as ins:
+            for line in ins:
+                if '==' in line:
+                    data = line.split('==')
+                    url_mappings[data[1].replace('\n', '')] = data[0]
+
+        if request.form['url'] in url_mappings.keys():
+            return BASE_URL + url_mappings[url]
+
+        short_url = ''.join(random.choices(string.ascii_lowercase + string.digits, k=5))
         try:
-            mode = 'w'
-            file = open(file_path, mode)
-            file.write(str(request.form['url']))
+            mode = 'a'
+            file = open(STORAGE_FILE, mode)
+            file.write(short_url+'=='+str(url+'\n'))
             file.close()
-            return BASE_URL + file_name
+            return BASE_URL + short_url
         except Exception as e:
             print("Error Message : " + str(e.message))
 
     return render_template('website/index.html')
 
 
-@website.route('/<filename>', methods=['GET', 'POST'])
-def redirect_url(filename):
+@website.route('/<url_key>', methods=['GET', 'POST'])
+def redirect_url(url_key):
     """
     Redirect the page to the encrypted url
     """
-    file_path = STORAGE_FOLDER + filename
 
     try:
-        with open(file_path) as file:
-            read_data = file.read()
-        return redirect(read_data)
+        with open(STORAGE_FILE, "r") as ins:
+            for line in ins:
+                data = line.split('==')
+                if data[0] == url_key:
+                    return redirect(data[1].replace('\n', ''))
     except Exception as e:
         print("Error Message : " + str(e.message))
 
